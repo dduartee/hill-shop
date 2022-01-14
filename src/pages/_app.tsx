34f858323baa0d16ab911b045f006b84f77f24d5
dduartee <gabrielkduarte234@gmail.com>
2022-01-14 15:08:52 -0300

    loadServiceWorker() is now called inside useEffect()

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,7 +17,9 @@ interface MyAppProps extends AppProps {
 
 export default function MyApp(props: MyAppProps) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
-  loadServiceWorker()
+  React.useEffect( () => {
+    loadServiceWorker()
+  }, []);
   
   return (
     <CacheProvider value={emotionCache}>
